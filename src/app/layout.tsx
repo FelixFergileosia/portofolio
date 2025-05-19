74c7756d04cc5a69f4335b769f51887f5ea43053
Felix Fergileosia <vincentsiusfelix@gmail.com>
2025-05-19 20:24:59 +0700

    fix: remove 'user' unused import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 // src/app/layout.tsx
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import { Home, Folder, User, Hexagon } from "lucide-react";
+import { Home, Folder, Hexagon } from "lucide-react";
 import Sidebar, { type NavItem } from "@/components/sidebar";
 import "./globals.css";
 
